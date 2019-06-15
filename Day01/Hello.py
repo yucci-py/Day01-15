157a04d4e36198945b4c456f07b5d6b36cbb0880
Yucciii <blogger.cabao@gmail.com>
2019-06-15 16:48:17 +0800

    Add files via upload

---
+++ Day01/Hello.py
@@ -0,0 +1,17 @@
+"""
+這是多行註解哦
+2019/06/15
+By 婷
+"""
+
+
+# 印出 Hello
+print('Hello')
+
+print('你好','哈哈')
+# 兩段字串用 , 分隔
+print('你好','哈哈', sep=', ', end='!')
+# 結尾換行
+print('Goodbye', end='!\n')
+
+print('Goodbye', end='!')
